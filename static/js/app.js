81674de079ffbe647b2892e15e59c123fd6e0212
peo_gcruz <cad_gcruz@uolinc.com>
2015-07-06 17:58:25 -0300

    oi

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,8 @@
 // Clicks
 var panelHeader = document.querySelectorAll(".panel-header");
 var panelBody 	= document.querySelectorAll(".panel-body");
+var play 	= document.querySelectorAll('.gameTiming');
+var timeoutTiming = 500;
 
 for (var i = 0; i < panelHeader.length; i++ ) {
 	panelHeader[i].addEventListener('click', function(e){
@@ -8,20 +10,48 @@ for (var i = 0; i < panelHeader.length; i++ ) {
 		var corpo = e.target.parentNode.parentNode.children[1];
 		var classes = corpo.className.split(' ');
 		var listaClasses = corpo.className;
-		console.log(classes[classes.length-1]);
 
 		if ( classes[classes.length-1] != "panel-show") {
 			corpo.className = corpo.className + " panel-show";
 		} else {
 			var a = classes.slice(0, classes.length-1).join(' ');
-			console.log("X=>" + a);
 			corpo.className = a;
 		}
 	});
 }
 
+for (var i = 0; i < play.length; i++ ) {
+	play[i].addEventListener('click', function(e) {
+		if (e.target.getAttribute("class") == "play") {
+			console.log("play");
+			timeoutTiming = 500;
+		} else {
+			console.log("pause");
+			timeoutTiming = 99999999999999999999;
+		}
+	});
+}
 
-// Start
+/// FUNCAO PARA o jogo
+var game = {
+	init: function() {
+		console.log('inicio');
+	},
+	continue: function() {
+		console.log('oi');
+	}
+}
+
+// Game Init
+game.init();
+
+// Items
+var timing = window.setTimeout(function(){
+  game.continue();
+}, timeoutTiming);
+
+///////////////////////////////////////////////////////////
+// Angular Start
 var app = angular.module("myApp", []);
 
 // Funcao para o arquivo
